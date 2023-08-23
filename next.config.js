d058cdd882227b2cdf2f86f808cbf49b27269b19
Louie Heaton <louieheaton@icloud.com>
2023-08-23 12:39:05 +0100

    init

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const path = require('path');
+const nextConfig = {
+    reactStrictMode: true,
+    webpack(config) {
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        });
+        return config;
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
